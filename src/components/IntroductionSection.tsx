24f29fd5afab3af5fe6467aea8516cfcff3a009a
ejoonko <ejoonko2003@gmail.com>
2023-09-26 14:53:13 +0200

    Added introduction section and navbar

---
+++ src/components/IntroductionSection.tsx
@@ -0,0 +1,7 @@
+export default function IntroductionSection() {
+  return (
+    <div className="default-max-width">
+      <p>Hello World!</p>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div className="h-8 bg-black"></div>
+    </div>
+  );
+}
